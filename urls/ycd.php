3e8da0a453ff45d5db4a3826ce422f5cc1434399
yancd <yanchendan@163.com>
2014-12-20 18:58:04 +0800

    ycd

--- urls/ycd.php
+++ urls/ycd.php
@@ -1,6 +1,14 @@
 <?php
 return array(
         '#^/login$#' => 'login',
-        '#^/signup$#' => 'signup'
-        
+        '#^/signup$#' => 'signup',
+		'#^/forgetpassword$#' => 'forgetpsd',
+		'#^/personjoin$#' => 'personjoin',
+		'#^/personjoinsuccess$#' => 'personjoinsuccess',
+		'#^/jointeamsuccess$#' => 'jointeamsuccess',
+		'#^/scanproject$#' => 'scanproject',
+		'#^/scanproject-1$#' => 'scanproject-1',
+		'#^/scanproject-2$#' => 'scanproject-2',
+		'#^/recomproject$#' => 'recomproject',
+		'#^/choseskill$#' => 'choseskill'
 );
